5b89c857d50cf0267d79769ca65e1e000e4682a1
jacksonnsaini <jsaini34@my.bcit.ca>
2024-05-23 22:48:16 -0700

    adding historical value html, css, and js pages, and images folder

---
+++ scripts/scriptHistorical.js
@@ -0,0 +1,20 @@
+document.getElementById("checkButton").addEventListener("click", function() {
+    const date = document.getElementById("date").value;
+    const currency1 = document.getElementById("startCurrency").value; // Get the start currency
+    const currency2 = document.getElementById("endCurrency").value; 
+    const apiUrl = `https://api.frankfurter.app/${date}?from=${currency1}&to=${currency2}`;
+    
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const rate = data.rates[currency2];
+        const formattedDate = new Date(date).toDateString();
+        const resultElement = document.getElementById("result");
+        resultElement.textContent = `On ${formattedDate}, 1 ${currency1} was equal to ${rate} ${currency2}.`;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        const resultElement = document.getElementById("result");
+        resultElement.textContent = 'An error occurred. Please try again.';
+      });
+  });
